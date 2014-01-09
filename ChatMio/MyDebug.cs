f41430ad797c749fb1bd099b14fa85b4ad3c37ba
Yuuto Tokunaga <yuntan.sub1@gmail.com>
2014-01-09 12:44:32 +0900

    MyDebugにnullチェック、説明文追加

--- ChatMio/MyDebug.cs
+++ ChatMio/MyDebug.cs
@@ -6,31 +6,31 @@ namespace ChatMio
 {
 	class MyDebug
 	{
-		private static bool _usingLogFile;
-		//public static void WriteLine (string msg, params object[] args)
-		//{
-		//	string message = String.Format(msg, args);
-		//	Debug.WriteLine(message);
-		//	WriteLog(message);
-		//}
+		private static bool _usingLogFile;								//ログファイルが使用中であることを示すフラグ
 
+		/// <summary>
+		/// デバッグ出力とログファイルにログを吐く
+		/// </summary>
+		/// <param name="obj">this or null</param>
+		/// <param name="msg">デバッグメッセージ</param>
+		/// <param name="args">代入文字列</param>
 		public static void WriteLine (object obj, string msg, params object[] args)
 		{
-			string message = String.Format("//{0}// {1}", obj, String.Format(msg, args));
-			Debug.WriteLine(message);
-			WriteLog(message);
+			string message = String.Format("//{0}// {1}", obj ?? "null", String.Format(msg, args));
+			Debug.WriteLine(message);									//デバッグ出力
+			WriteLog(message);											//ログファイルに追記
 		}
 
 		private static void WriteLog (string message)
 		{
 			string fileName = String.Format("{0}.log", DateTime.Now.ToString("yyMMdd"));
 
-			if (!_usingLogFile) {
-				_usingLogFile = true;
+			if (!_usingLogFile) {										//ログファイルが使用中でなければ
+				_usingLogFile = true;									//使用中フラグを立てる
 				using (var w = new StreamWriter(fileName, true)) {
 					w.WriteLine("{0}  {1}", DateTime.Now.ToString("HH:mm:ss"), message);
 				}
-				_usingLogFile = false;
+				_usingLogFile = false;									//使用中フラグを下ろす
 			}
 		}
 	}
