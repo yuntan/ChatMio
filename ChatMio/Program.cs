dd01d7ca658155221aafa8c7628fed38f03af305
Yuuto Tokunaga <yuntan.sub1@gmail.com>
2014-01-14 16:19:59 +0900

    プログラムの複数自動起動を禁止

--- ChatMio/Program.cs
+++ ChatMio/Program.cs
@@ -3,6 +3,7 @@ using System.Windows.Forms;
 using System.IO;
 using System.Diagnostics;
 using System.Xml;
+using System.Threading;
 
 namespace ChatMio
 {
@@ -19,29 +20,37 @@ namespace ChatMio
 		[STAThread]
 		static void Main ()
 		{
-			Exit = false;													//終了フラグの初期値
-
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-
-			//GenerateTestData(1000);
-
-			while (true) {													//無限ループ
-				var loginForm = new LoginForm();							
-				DialogResult login = loginForm.ShowDialog();				//ログインフォームを表示
-
-				if (login == DialogResult.OK) {								//ログイン成功時
-					Application.Run(new ChatForm());						//ChatForm表示
-				}
-				else if (login == DialogResult.Cancel) {					//登録ボタン押下時
-					var registerForm = new RegisterForm();
-					registerForm.ShowDialog();								//RegisterForm表示
-				}
-				else if (login == DialogResult.Abort) {						//×ボタン押下時
-					return;													//終了
+			using (var mtx = new Mutex(false, "ChatMio")) {
+				if (!mtx.WaitOne(0, false)) {
+					MessageBox.Show("2つ以上のChatMioを同時に使うことはできません。",
+							"複数起動禁止", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+					return;
 				}
 
-				if (Exit == true) { break; }								//フラグが立っていたら終了する
+				Exit = false;													//終了フラグの初期値
+
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);
+
+				//GenerateTestData(1000);
+
+				while (true) {													//無限ループ
+					var loginForm = new LoginForm();							
+					DialogResult login = loginForm.ShowDialog();				//ログインフォームを表示
+
+					if (login == DialogResult.OK) {								//ログイン成功時
+						Application.Run(new ChatForm());						//ChatForm表示
+					}
+					else if (login == DialogResult.Cancel) {					//登録ボタン押下時
+						var registerForm = new RegisterForm();
+						registerForm.ShowDialog();								//RegisterForm表示
+					}
+					else if (login == DialogResult.Abort) {						//×ボタン押下時
+						break;													//終了
+					}
+
+					if (Exit == true) { break; }								//フラグが立っていたら終了する
+				}
 			}
 		}
 
